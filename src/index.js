b64b531c8e6747a8a4380c335a8787378aa75dbf
Lee EC <lyxuncle@gmail.com>
2019-08-14 20:59:41 +0800

    feat: 新增上报

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './css/base.scss'
 import './css/package.scss'
 
+import Report from '@o2team/ambient-report'
+
 import { O2_AMBIENT_CONFIG } from './js/utils/const'
 import initAmbient from './js/ambient'
 
@@ -14,3 +16,6 @@ try {
 } catch (e) {
   console.log(e)
 }
+
+Report.init({})
+Report.processPV()
